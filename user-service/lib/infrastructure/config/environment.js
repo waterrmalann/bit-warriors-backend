9bd116618417abe07ec9b5169ecf22417df02b87
Alan <56948680+waterrmalann@users.noreply.github.com>
2023-11-15 15:36:31 +0530

    consistent environment

--- user-service/lib/infrastructure/config/environment.js
+++ user-service/lib/infrastructure/config/environment.js
@@ -2,9 +2,7 @@ export default (() => {
     console.log("[env] Environment variables have been injected from `.env`");
 
     const env = {
-        DATABASE: {
-            URL: process.env.DATABASE_URI || '',
-        },
+        DATABASE_URL: process.env.DATABASE_URL,
         JWT_SECRET: process.env.JWT_SECRET,
         RESEND_API_KEY: process.env.RESEND_API_KEY
     };
