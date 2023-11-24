3666acce407abb0bce760cbff5264106bbf7c532
Alan <56948680+waterrmalann@users.noreply.github.com>
2023-11-24 14:14:51 +0530

    message broker interface

---
+++ user-service/lib/infrastructure/repositories/MessageBrokerRepositoryKafka.js
@@ -0,0 +1,17 @@
+'use strict';
+
+import IMessageBrokerRepository from "../../domain/repositories/IMessageBrokerRepository";
+
+export default class extends IMessageBrokerRepository {
+    constructor() {
+
+    }
+
+    consumeMessage(topic, callback) {
+
+    }
+
+    produceMessage(topic, message) {
+
+    }
+}
