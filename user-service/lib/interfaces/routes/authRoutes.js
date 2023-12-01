703b5d0e6c4bf12bfeec20eb98fdde3e156eb70d
Alan <56948680+waterrmalann@users.noreply.github.com>
2023-12-02 00:01:23 +0530

    feat(auth): add actual data

--- user-service/lib/interfaces/routes/authRoutes.js
+++ user-service/lib/interfaces/routes/authRoutes.js
@@ -3,7 +3,7 @@ import { LoginUser, LoginUserMFA, LogoutUser, RegisterUser, VerifyEmail } from '
 import { isAuthenticated } from '../controllers/AuthMiddleware.js';
 const router = express.Router();
 
-router.get('/test', (req, res) => { 
+router.get('/test', (req, res) => {
     console.log(req.headers);
     res.send("API is working")
 });
@@ -13,6 +13,29 @@ router.post('/login-mfa', LoginUserMFA);
 router.get('/verify/:verificationCode', VerifyEmail);
 router.get('/logout', LogoutUser);
 
-router.get('/auth', isAuthenticated, (req, res) => { res.json(req.user) });
+router.get('/auth', isAuthenticated, (req, res) => {
+    let userObject = {
+        id: req.user.id,
+        username: req.user.username,
+        email: req.user.email,
+        emailVerified: req.user.emailVerified,
+        followers: req.user.followers,
+        following: req.user.following,
+        createdAt: req.user.createdAt,
+        mfa: req.user.mfa,
+        totalScore: req.user.totalScore,
+        totalSubmissions: req.user.totalSubmissions,
+        totalActiveDays: req.user.totalActiveDays,
+        currentActiveDays: req.user.currentActiveDays,
+        maxActiveDays: req.user.maxActiveDays,
+        clan: req.user.clan,
+        bio: req.user.bio,
+        githubUsername: req.user.githubUsername,
+        linkedInUsername: req.user.linkedInUsername,
+        xUsername: req.user.xUsername,
+        personalWebsite: req.user.personalWebsite
+    };
+    res.json(userObject);
+});
 
 export default router;
