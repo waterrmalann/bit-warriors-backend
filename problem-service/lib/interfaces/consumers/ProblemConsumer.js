6691ae9b0d54b40a700e2e7beb6d63f5e241b08d
Alan <56948680+waterrmalann@users.noreply.github.com>
2023-12-13 20:30:25 +0530

    feat(ack): add kafka acknowledgement

--- problem-service/lib/interfaces/consumers/ProblemConsumer.js
+++ problem-service/lib/interfaces/consumers/ProblemConsumer.js
@@ -7,11 +7,13 @@ import ProblemRepository from "../../infrastructure/repositories/ProblemReposito
 const problemRepository = new ProblemRepository();
 
 const consumer = kafka.consumer({ groupId: 'problem-consumer-group' });
+const producer = kafka.producer();
 
 const run = async () => {
 
     await consumer.connect();
     console.log("💬 Established connection with Kafka.");
+    await producer.connect();
 
     await consumer.subscribe({ topics: ['PROBLEM_CREATION', 'PROBLEM_UPDATION', 'PROBLEM_DELETION'] });
     console.log("\t - Subscribed to PROBLEM_CREATION, PROBLEM_UPDATION, PROBLEM_DELETION");
@@ -19,24 +21,45 @@ const run = async () => {
     await consumer.run({
         eachMessage: async ({ topic, partition, message }) => {
             const data = JSON.parse(message.value.toString());
+            let acknowledgeProblemId = null;
             switch (topic) {
                 case "PROBLEM_CREATION": {
                     console.log("[PROBLEM_CREATION] A new problem was created.");
                     await AddProblemInteractor(data, { problemRepository });
+                    acknowledgeProblemId = data.problemId;
                     break;
                 }
                 case "PROBLEM_UPDATION": {
                     console.log("[PROBLEM_UPDATION] A problem was updated.");
                     await EditProblemInteractor(data.id, data.data, { problemRepository });
+                    acknowledgeProblemId = data.id;
                     break;
                 }
                 case "PROBLEM_DELETION": {
                     console.log("[PROBLEM_DELETION] A problem was deleted.");
                     await DeleteProblemInteractor(data.id, { problemRepository });
+                    acknowledgeProblemId = data.id;
                     break;
                 }
             }
-            console.log(data);
+
+            const ackMessage = {
+                key: 'problem_crud_ack', 
+                value: JSON.stringify({ 
+                    problemId: acknowledgeProblemId, 
+                    topic: topic, 
+                    timestamp: Date.now() 
+                }) 
+            };
+
+            try {
+                await producer.send({
+                    topic: 'PROBLEM_ACK',
+                    messages: [ackMessage],
+                });
+            } catch (err) {
+                console.error(err);
+            }
         },
     });
 };
