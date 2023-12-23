ca5e8d93b4e783e0370ee81f37756ae6c15019f8
Alan <56948680+waterrmalann@users.noreply.github.com>
2023-12-23 18:33:31 +0530

    refactor: add jsdoc

--- submission-service/lib/domain/services/ITestService.js
+++ submission-service/lib/domain/services/ITestService.js
@@ -3,7 +3,19 @@ export default class ITestService {
         throw new Error("method evaluate not implemented.");
     }
 
-    runTests(language, code, testCases) {
+    /**
+     * Runs given testcases on submitted code and returns the result.
+     * @param {string} language - The language the code uses.
+     * @param {string} code - The submitted code.
+     * @param {{
+     *       preloadedCode: string;
+     *       functionName: string;
+     *       testCases: object[];
+     * }} problem - An object containing information about the problem and test cases.
+     *
+     * @returns {Promise<object>}
+    */
+    runTests(language, code, { preloadedCode, functionName, testCases }) {
         throw new Error("method runTests not implemented.");
     }
 }
