c36bf9fa09b52440a1e9ba7bde29bba7487b9580
Alan <56948680+waterrmalann@users.noreply.github.com>
2024-01-02 11:08:59 +0530

    fix: dockerize user-service :dolphin:

--- user-service/lib/infrastructure/config/environment.js
+++ user-service/lib/infrastructure/config/environment.js
@@ -5,9 +5,11 @@ export default (() => {
     console.log("[env] Environment variables have been injected from `.env`");
 
     const env = {
+        PORT: process.env.PORT,
         DATABASE_URL: process.env.DATABASE_URL,
         JWT_SECRET: process.env.JWT_SECRET,
         RESEND_API_KEY: process.env.RESEND_API_KEY,
+
         GITHUB_OAUTH_CLIENT_ID: process.env.GITHUB_OAUTH_CLIENT_ID,
         GITHUB_OAUTH_CLIENT_SECRET: process.env.GITHUB_OAUTH_CLIENT_SECRET,
 
